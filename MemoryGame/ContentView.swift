1669636f2da6199f04cb82d055b3a7d2568e32a8
Nathan <42325916+nathlemma@users.noreply.github.com>
2025-08-23 23:17:35 -0400

    Initial Commit

---
+++ MemoryGame/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MemoryGame
+//
+//  Created by dev on 2025-08-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("H")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by dev on 2025-08-23.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
